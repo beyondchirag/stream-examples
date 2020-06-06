f08dbe6e131c97490a2a2f4ea38df969546bf2c1
Chirag <Chirag@192.168.1.13>
2020-06-06 23:32:23 +0530

    Added examples

--- src/com/examples/MiscExamples.java
+++ src/com/examples/MiscExamples.java
@@ -3,6 +3,8 @@ package com.examples;
 import static com.examples.utils.Utils.getRandomPersons;
 import static com.examples.utils.Utils.outputSeparator;
 
+import java.util.function.Function;
+
 public class MiscExamples {
 	public static void main(String[] args) {
 //		Calculate average age
@@ -18,8 +20,16 @@ public class MiscExamples {
 				.anyMatch(person -> person.getAge()> 5));
 		outputSeparator();
 		
-		
-		
+//		Reduce - accumulator
+		System.out.println(getRandomPersons(10)
+			.stream()
+				.reduce((person1, person2) -> {
+					if (person1.getAge() > person2.getAge()) {
+						return person1;
+					} else {
+						return person2;
+					}}));
+		outputSeparator();
 		
 	}
 }
