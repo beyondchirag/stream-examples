0bed4fdb67d59a94ed8720c6061e6341fc4a6caa
Chirag <Chirag@192.168.1.13>
2020-06-06 21:51:57 +0530

    Some stream examples

--- src/com/examples/PersonData.java
+++ src/com/examples/PersonData.java
@@ -1,5 +1,6 @@
 package com.examples;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -9,16 +10,26 @@ import com.examples.pojo.Person;
 
 public class PersonData {
 
-	public static List<Person> getPersons() {
-		 return IntStream.range(1, 100).mapToObj(i -> {return new Person("fName_"+i,"lName_"+i,random5DigitPhoneNumber(),i);}).collect(Collectors.toList());
+	public static List<Person> getRandomPersons(int noOfPersons) {
+		 return IntStream.rangeClosed(1, noOfPersons).parallel().mapToObj(i -> {return new Person("fName_"+i,"lName_"+i,random5DigitPhoneNumber(),i);}).collect(Collectors.toList());
 	}
 
+	public static List<Person> getPersons() {
+
+		List<Person> persons = new ArrayList<Person>();
+		
+		persons.add(new Person("First0","Last0","123-456-7890",1));
+		persons.add(new Person("First0","Last1","123-456-7891",2));
+		persons.add(new Person("First1","Last1","123-456-7890",3));
+		persons.add(new Person("First1","Last0","123-456-7891",3));
+		return persons;
+		
+	}
+	
+	
 	private static String random5DigitPhoneNumber() {
 		Random ran = new Random();
 		return ran.nextInt(100000)+"";
 		
-	}
-	public static void main(String[] args) {
-		getPersons().forEach(System.out::println);
 	}
 }
--- src/com/examples/StreamExamples.java
+++ src/com/examples/StreamExamples.java
@@ -1,9 +1,77 @@
 package com.examples;
 
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import com.examples.pojo.Person;
+
 public class StreamExamples {
 	
 	
+	private static void printSeparatorLine() {
+		System.out.println("-----------------------\n\n");
+	}
+	
+	
 	public static void main(String[] args) {
-		PersonData.getPersons().stream().filter(person -> person.getAge()%2==0).forEach(System.out::println);
+		
+//		Stream personlist and filter
+		PersonData.getRandomPersons(10)
+			.stream()
+			.filter(person -> person.getAge()%2==0)
+			.forEach(System.out::println);
+		printSeparatorLine();
+		
+//		Stream parallel - un ordered output
+		PersonData.getRandomPersons(10)
+			.parallelStream()
+			.forEach(System.out::println);
+		printSeparatorLine();
+		
+//		Stream parallel - ordered output
+		PersonData.getRandomPersons(10)
+			.parallelStream()
+			.forEachOrdered(System.out::println);
+		printSeparatorLine();
+		
+//		Stream and pass comparators - comparing on two fields age and then phones numbers
+		System.out.println(PersonData.getRandomPersons(10)
+				.stream()
+				.sorted(Comparator.comparing(Person::getAge).thenComparing(Person::getPhoneNumber))
+				.collect(Collectors.toList()));
+		printSeparatorLine();
+		
+//		Calculate average age
+		System.out.println(PersonData.getRandomPersons(10)
+			.stream()
+			.mapToInt(person -> person.getAge())
+			.average());
+		printSeparatorLine();
+		
+//		Collect List as Map - Funtion.Identity refers to this
+		System.out.println(PersonData.getRandomPersons(10)
+			.stream()
+			.collect(Collectors.toMap(Person::getFirstName, Function.identity())));
+		printSeparatorLine();
+		
+//		Collect and filter List as Map - Funtion.Identity refers to this
+		System.out.println(PersonData.getRandomPersons(10)
+			.stream()
+			.filter(person -> person.getAge()%2==0)
+			.collect(Collectors.toMap(Person::getFirstName, Function.identity())));
+		printSeparatorLine();
+		
+//		Groups Person based on firstname - > output Map<Firstname, List<Person>>
+		System.out.println(PersonData.getPersons()
+				.stream()
+				.collect(Collectors.groupingBy(Person::getFirstName)));
+		printSeparatorLine();
+
+//		Groups Person based on firstname - > output Map<Firstname, List<Person> - size>
+		System.out.println(PersonData.getPersons()
+				.stream()
+				.collect(Collectors.groupingBy(Person::getFirstName, Collectors.counting())));
+		printSeparatorLine();
 	}
 }
